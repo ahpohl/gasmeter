2e6f9c94d784144cbc66fac571303e92c62be8a5
Alexander Pohl <alex@ahpohl.com>
2019-12-01 19:50:37 +0100

    gasmeter ready for rrdtool

--- resources/gasmeter.gnuplot
+++ resources/gasmeter.gnuplot
@@ -2,14 +2,14 @@ set datafile separator ","
 set terminal png size 900,400
 set title "Magnetometer"
 set xdata time
-set timefmt "%s"
+set timefmt "%Y-%m-%d %H:%M:%S"
 set format x "%H:%M"
 set xlabel "Time (hh:mm)"
 set ylabel "Magnetic field (10^{-7} T)"
 set key top right
 set grid
-#set xrange ["09:30":"09:35"]
-plot "mag.txt" using ($1+7200):2 with lines lw 2 lt 2 lc rgb "black" title 'B', \
-"mag.txt" using ($1+7200):3 with lines lw 2 lt 2 lc rgb "red" title 'B_{x}', \
-"mag.txt" using ($1+7200):4 with lines lw 2 lt 2 lc rgb "blue" title 'B_{y}', \
-"mag.txt" using ($1+7200):5 with lines lw 2 lt 2 lc rgb "cyan" title 'B_{z}'
+set xrange ["2019-12-01 18:00":"2019-12-01 18:20"]
+plot "mag.txt" using 1:5 with lines lw 2 lt 2 lc rgb "black" title 'B', \
+ "mag.txt" using 1:2 with lines lw 2 lt 2 lc rgb "red" title 'B_{x}', \
+ "mag.txt" using 1:3 with lines lw 2 lt 2 lc rgb "blue" title 'B_{y}', \
+ "mag.txt" using 1:4 with lines lw 2 lt 2 lc rgb "cyan" title 'B_{z}'
--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,62 @@
 #include <ctime>
 #include <chrono>
 #include <thread>
+#include <cstring>
 #include <unistd.h>
 #include <mag3110>
+#include <wiringPi.h>
+#include <errno.h>
 
 using namespace std;
 
+int const MAG3110_PIN = 7;
+static volatile bool isEvent = false;
+void magISR(void)
+{
+  isEvent = true;
+}
+
 int main(int argc, char** argv)
 {
+  if (wiringPiSetup() < 0)
+  {
+    throw runtime_error(string("Unable to setup wiringPi: ") + 
+      + strerror(errno) + " (" + to_string(errno) + ")");
+  }
+  if (wiringPiISR(MAG3110_PIN, INT_EDGE_RISING, &magISR) < 0)
+  {
+    throw runtime_error(string("Unable to setup ISR: ") + 
+      + strerror(errno) + " (" + to_string(errno) + ")");
+  }
+
   MAG3110 mag;
+  mag.getVersion();
   mag.initialize("/dev/i2c-1");
   mag.reset();
+  mag.setDR_OS(MAG3110::MAG3110_DR_OS_0_63_128);
   mag.start();
-
+  
   ofstream file;
   file.open("mag.txt", ios::app);
-  time_t timestamp;
+  time_t rawtime;
+  struct tm* timeinfo;
+  char buffer[80];
   double scalarMag;
   int bx, by, bz;
 
   while (true) {
+    while (!isEvent) {
+      this_thread::sleep_for(chrono::milliseconds(1));
+    }
     mag.getMag(&bx, &by, &bz);
     scalarMag = mag.getMagnitude(bx, by, bz);
     mag.displayMag(bx, by, bz, scalarMag);
-    timestamp = time(nullptr);
-    file << timestamp << "," << bx << "," << by << "," << bz 
-      << "," << scalarMag << endl;
-    this_thread::sleep_for(chrono::seconds(1));
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    strftime(buffer, sizeof(buffer), "%Y-%m-%d %H:%M:%S", timeinfo);
+    file << string(buffer) << "," 
+      << bx << "," << by << "," << bz << "," << scalarMag << endl;
+    isEvent = false;
   }
   file.close();
 
