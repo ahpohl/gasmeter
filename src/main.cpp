277bd0baacb935b8fb4e93921641135c0a5dfa10
Alexander Pohl <alex@ahpohl.com>
2021-10-06 22:23:52 +0200

    publish every 5 seconds

--- src/main.cpp
+++ src/main.cpp
@@ -92,7 +92,8 @@ int main(int argc, char* argv[])
 
   while (shutdown == false)
   {
-    std::this_thread::sleep_for(std::chrono::milliseconds(40));
+    //std::this_thread::sleep_for(std::chrono::milliseconds(40));
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 	  if (!meter->Receive())
 	  {
       if (timeout < 5)
