a08005165d19da1760158d11ab6fcb966be1889e
Alexander Pohl <alex@ahpohl.com>
2022-01-08 17:54:16 +0100

    pg: use experimental time bucket function

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 # Gasmeter changelog
 
+## v0.4.4 - 2022-01-xx
+* TimescaleDB: replaced \_time\_bucket() with timescaledb\_experimental.time\_bucket\_ng()
+* created yearly\_view
+
 ## v0.4.3 - 2022-01-03
 * initial release
 * add readme
--- resources/postgres/pg_cron.sql
+++ resources/postgres/pg_cron.sql
@@ -8,11 +8,16 @@ SELECT cron.schedule('purge_log', '13 0 * * *', $$DELETE
     WHERE end_time < now() - INTERVAL '3 days'$$);
 
 -- schedule updates
-SELECT cron.schedule('gasmeter_daily', '2,17,32,47 * * * *', $$REFRESH
+SELECT cron.schedule('gasmeter_daily', '1,16,31,46 * * * *', $$REFRESH
   MATERIALIZED VIEW CONCURRENTLY daily_view$$);
 
-SELECT cron.schedule('gasmeter_monthly', '3,18,33,48 * * * *', $$REFRESH
+SELECT cron.schedule('gasmeter_monthly', '1,16,31,46 * * * *', $$REFRESH
   MATERIALIZED VIEW CONCURRENTLY monthly_view$$);
 
+SELECT cron.schedule('gasmeter_yearly', '1,16,31,46 * * * *', $$REFRESH
+  MATERIALIZED VIEW CONCURRENTLY yearly_view$$);
+
 UPDATE cron.job SET database = 'gasmeter' WHERE 
-  jobname = 'gasmeter_daily' OR jobname = 'gasmeter_monthly';
+  jobname = 'gasmeter_daily' OR 
+  jobname = 'gasmeter_monthly' OR
+  jobname = 'gasmeter_yearly';
